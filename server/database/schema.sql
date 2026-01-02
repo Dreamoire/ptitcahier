4b3b047837ff49328f0610026b224ad493b1353d
Emericodes <pro.emeric.m@gmail.com>
2026-01-02 12:17:00 +0100

    Update database and review PR, create Seeder files

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -2,15 +2,16 @@ CREATE TABLE school (
     id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
-    name VARCHAR(100) NOT NULL
+    school_name VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE parent (
     id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
-    first_name VARCHAR(100) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE
+    first_name VARCHAR(100) NOT NULL
+    
 );
 
 CREATE TABLE announcement_category (
@@ -23,36 +24,37 @@ CREATE TABLE ticket_category (
     name VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE class (
+CREATE TABLE classroom (
     id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    classroom_name VARCHAR(100) NOT NULL,
     school_id INT UNSIGNED NOT NULL,
-    name VARCHAR(100) NOT NULL,
     FOREIGN KEY (school_id) REFERENCES school(id)
 );
 
 CREATE TABLE announcement (
     id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    school_id INT UNSIGNED NOT NULL,         
     title VARCHAR(255) NOT NULL,
-    body_message TEXT NOT NULL,              
+    content TEXT NOT NULL, 
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,        
     announcement_category_id INT UNSIGNED NOT NULL,
-    FOREIGN KEY (school_id) REFERENCES school(id),
-    FOREIGN KEY (announcement_category_id) REFERENCES announcement_category(id)
+    school_id INT UNSIGNED NOT NULL,
+    FOREIGN KEY (announcement_category_id) REFERENCES announcement_category(id),
+    FOREIGN KEY (school_id) REFERENCES school(id)
 );
 
 CREATE TABLE student (
     id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
     last_name VARCHAR(100) NOT NULL,
     first_name VARCHAR(100) NOT NULL,
-    parent_id INT unsigned NOT NULL,
     born_at DATE NOT NULL,
-    class_id INT unsigned NOT NULL,
-    FOREIGN KEY (parent_id) REFERENCES parent(id),
-    FOREIGN KEY (class_id) REFERENCES class(id)
+    classroom_id INT unsigned NOT NULL,
+    parent_id INT unsigned NOT NULL,
+    FOREIGN KEY (class_id) REFERENCES class(id),
+    FOREIGN KEY (parent_id) REFERENCES parent(id)
+    
 );
 
-CREATE TABLE target_student (
+CREATE TABLE announcement_student (
     announcement_id INT UNSIGNED NOT NULL,
     student_id INT UNSIGNED NOT NULL,
     PRIMARY KEY (announcement_id, student_id),
@@ -62,18 +64,33 @@ CREATE TABLE target_student (
 
 CREATE TABLE ticket (
     id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    content TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    ticket_category_id INT UNSIGNED NOT NULL,
     parent_id INT UNSIGNED NOT NULL,
-    body_message TEXT NOT NULL,
-    FOREIGN KEY (ticket_category_id) REFERENCES ticket_category(id),
-    FOREIGN KEY (parent_id) REFERENCES parent(id)
+    ticket_category_id INT UNSIGNED NOT NULL,
+    FOREIGN KEY (parent_id) REFERENCES parent(id),
+    FOREIGN KEY (ticket_category_id) REFERENCES ticket_category(id)
 );
 
-CREATE TABLE concerned_student ( 
+CREATE TABLE ticket_student ( 
     ticket_id INT UNSIGNED NOT NULL,
     student_id INT UNSIGNED NOT NULL,
     PRIMARY KEY (ticket_id, student_id),
     FOREIGN KEY (ticket_id) REFERENCES ticket(id),
     FOREIGN KEY (student_id) REFERENCES student(id)
 );
+
+INSERT INTO announcement_category (name)
+VALUES
+("Vie de l'école"),
+("Administratif"),
+("Evénement");
+
+INSERT INTO ticket_category (name)
+VALUES
+("Absence"),
+("Autorisation"),
+("Information urgente"),
+("Demande d'information");
+
+
