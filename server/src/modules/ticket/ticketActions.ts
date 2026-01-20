894291ff0dc0a3487931ecc14f4e720e4e99812b
Yuliana Krasulya <krasulyayuliana@gmail.com>
2026-01-20 11:09:42 +0100

    modules: ticketActions + ticketRepository

--- server/src/modules/ticket/ticketActions.ts
+++ server/src/modules/ticket/ticketActions.ts
@@ -1,20 +1,14 @@
 import type { RequestHandler } from "express";
-import { StatusCodes } from "http-status-codes";
 
 import ticketRepository from "./ticketRepository";
 
 const browseBySchool: RequestHandler = async (req, res, next) => {
   try {
-    const schoolId = 999;
+    const schoolId = 1;
 
     const tickets = await ticketRepository.readAllBySchool(schoolId);
 
-    if (tickets.length === 0) {
-      res.sendStatus(StatusCodes.NOT_FOUND);
-      return;
-    }
-
-    res.status(StatusCodes.OK).json(tickets);
+    res.json(tickets);
   } catch (err) {
     next(err);
   }
--- server/src/modules/ticket/ticketRepository.ts
+++ server/src/modules/ticket/ticketRepository.ts
@@ -6,27 +6,33 @@ class TicketRepository {
   async readAllBySchool(schoolId: number) {
     const [rows] = await databaseClient.query<Rows>(
       `
-      SELECT
+ SELECT
         t.id,
         t.content,
         t.created_at,
         tc.name AS category_name,
         p.first_name AS parent_first_name,
-        p.last_name AS parent_last_name
-      FROM ticket AS t
-      JOIN ticket_category AS tc
-        ON t.ticket_category_id = tc.id
-      JOIN parent AS p
-        ON t.parent_id = p.id
-      WHERE EXISTS (
-        SELECT 1
-        FROM student AS s
-        JOIN classroom AS c
-          ON c.id = s.classroom_id
-        WHERE s.parent_id = p.id
-          AND c.school_id = ?
-      )
-      ORDER BY t.created_at DESC
+        p.last_name AS parent_last_name,
+        tic_stu.ticket_id,
+    GROUP_CONCAT(
+        CONCAT(s.first_name, ' ', s.last_name)
+        ORDER BY s.first_name
+        SEPARATOR ', '
+    ) AS student_names
+    FROM ticket AS t
+    JOIN ticket_category AS tc
+    ON t.ticket_category_id = tc.id
+    JOIN parent AS p
+    ON t.parent_id = p.id
+    JOIN ticket_student AS tic_stu
+    ON tic_stu.ticket_id = t.id
+    JOIN student AS s
+    ON tic_stu.student_id = s.id
+    JOIN classroom AS c
+    ON s.classroom_id = c.id
+    WHERE c.school_id = ?
+    GROUP BY t.id
+    ORDER BY t.created_at ASC
       `,
       [schoolId],
     );
