46e38284afafcf86cd0858a100105d84b736c30a
Yuliana Krasulya <krasulyayuliana@gmail.com>
2026-02-06 14:27:04 +0100

    avant le merge avec le dev

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router";
+
 import ParentLayout from "./layouts/ParentLayout";
 import SchoolLayout from "./layouts/SchoolLayout";
+
 import AnnouncementsParentView from "./pages/announcement/AnnouncementsParentView";
 import HomeParentView from "./pages/home/HomeParentView";
 import TicketNew from "./pages/ticket/TicketNew";
@@ -8,16 +10,22 @@ import Tickets from "./pages/ticket/Tickets";
 
 const router = createBrowserRouter([
   {
-    path: "/parent/home",
-    element: <HomeParentView />,
-  },
-  {
-    path: "/parent/announcements",
-    element: <AnnouncementsParentView />,
-  },
-  {
-    path: "/parent/tickets/new",
-    element: <TicketNew />,
+    path: "/parent",
+    element: <ParentLayout />,
+    children: [
+      {
+        path: "home",
+        element: <HomeParentView />,
+      },
+      {
+        path: "announcements",
+        element: <AnnouncementsParentView />,
+      },
+      {
+        path: "tickets/new",
+        element: <TicketNew />,
+      },
+    ],
   },
   {
     path: "/school",
@@ -26,16 +34,6 @@ const router = createBrowserRouter([
       {
         path: "tickets",
         element: <Tickets />,
-      },
-    ],
-  },
-  {
-    path: "/parent",
-    element: <ParentLayout />,
-    children: [
-      {
-        path: "announcements",
-        element: <AnnouncementsParentView />,
       },
     ],
   },
